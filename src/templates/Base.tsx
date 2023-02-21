8e7b803b61a24f02d169465bfe2f3b196b143252
Guntur KH <guntur.kh@gmail.com>
2023-02-22 02:04:25 +0700

    feat: change globe

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,21 +1,23 @@
+/* eslint-disable global-require */
 /* eslint-disable unused-imports/no-unused-vars */
 /* eslint-disable @typescript-eslint/naming-convention */
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
-import dynamic from 'next/dynamic';
+import { GlobeMethods } from 'react-globe.gl';
+import { SizeMe } from 'react-sizeme';
 
 import { Hero } from './Hero';
 import bg from '../../public/assets/images/newbg.png';
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
 
-const ReactGlobe = dynamic(() => import('react-globe'), {
-  ssr: false,
-});
+let Globe: any = () => null;
+if (typeof window !== 'undefined') Globe = require('react-globe.gl').default;
 
 const Base = () => {
+  const globeEl = useRef<GlobeMethods>();
   // eslint-disable-next-line unused-imports/no-unused-vars
-  const [markers, _] = useState([
+  const [places, _] = useState([
     {
       id: 'marker1',
       city: 'Singapore',
@@ -39,57 +41,51 @@ const Base = () => {
       value: 50,
     },
   ]);
-  // const [event, setEvent] = useState<any>(null);
-  // const [details, setDetails] = useState<any>(null);
 
-  function markerTooltipRenderer(marker: any) {
-    return `CITY: ${marker.city} (Value: ${marker.value})`;
-  }
-
-  const options = {
-    markerTooltipRenderer,
-  };
-
-  // function onClickMarker(marker: any, markerObject: any, markerEvent: any) {
-  function onClickMarker() {
-    // setEvent({
-    //   type: 'CLICK',
-    //   marker,
-    //   markerObjectID: markerObject.uuid,
-    //   pointerEventPosition: { x: markerEvent.clientX, y: markerEvent.clientY },
-    // });
-    // setDetails(markerTooltipRenderer(marker));
-  }
-
-  // function onDefocus(previousFocus: any) {
-  function onDefocus() {
-    // setEvent({
-    //   type: 'DEFOCUS',
-    //   previousFocus,
-    // });
-    // setDetails(null);
-  }
   return (
     <div
       className="antialiased text-gray-600"
       style={{
         backgroundImage: `url(${bg.src})`,
-        width: '100%',
-        height: '100%',
         backgroundSize: 'cover',
+        width: '100vw',
+        height: '100vh',
       }}
     >
       <Meta title={AppConfig.title} description={AppConfig.description} />
       <Hero />
-      <ReactGlobe
-        height="90vh"
-        globeBackgroundTexture={null}
-        //  @ts-ignore
-        markers={markers}
-        options={options}
-        onClickMarker={onClickMarker}
-        onDefocus={onDefocus}
-      />
+      <SizeMe>
+        {({ size: { width } }) => (
+          <Globe
+            ref={globeEl}
+            onGlobeReady={() => {
+              const MAP_CENTER = {
+                lat: -13.3766016,
+                lng: 127.342919,
+                altitude: 1.5,
+              };
+              if (globeEl && globeEl.current) {
+                globeEl?.current?.pointOfView(MAP_CENTER, 100);
+              }
+            }}
+            globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+            width={width as number}
+            backgroundColor="rgba(0,0,0,0)"
+            onLabelClick={(e: any) => {
+              // eslint-disable-next-line no-alert
+              window.alert(`label click ${JSON.stringify(e)}`);
+            }}
+            labelsData={places}
+            labelLat={(d: any) => (d as any)?.coordinates[0]}
+            labelLng={(d: any) => (d as any)?.coordinates[1]}
+            labelText={(d: any) => (d as any).city}
+            labelSize={() => 2}
+            labelDotRadius={() => 2}
+            labelColor={() => 'rgba(255, 0, 0, 1)'}
+            labelResolution={2}
+          />
+        )}
+      </SizeMe>
       {/* <VerticalFeatures /> */}
       {/* <Banner /> */}
       {/* <Footer /> */}
