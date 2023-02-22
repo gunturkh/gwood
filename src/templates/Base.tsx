72989a10ee20ecbd0d13e1644bb24109b790cd9d
Guntur KH <guntur.kh@gmail.com>
2023-02-23 00:30:16 +0700

    feat: update marker style

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -16,29 +16,39 @@ if (typeof window !== 'undefined') Globe = require('react-globe.gl').default;
 
 const Base = () => {
   const globeEl = useRef<GlobeMethods>();
+  const markerSvg = `<svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M9.68193 25.0525C2.83316 16.2469 9.10822 3.41669 20.2637 3.41669C31.4191 3.41669 37.6942 16.2469 30.8454 25.0525L20.2637 38.6576L9.68193 25.0525ZM20.2636 22.2084C23.0941 22.2084 25.3886 19.9138 25.3886 17.0834C25.3886 14.2529 23.0941 11.9584 20.2636 11.9584C17.4332 11.9584 15.1386 14.2529 15.1386 17.0834C15.1386 19.9138 17.4332 22.2084 20.2636 22.2084Z" fill="#CD521A"/>
+  </svg>
+  `;
   // eslint-disable-next-line unused-imports/no-unused-vars
   const [places, _] = useState([
     {
       id: 'marker1',
       city: 'Singapore',
-      color: 'red',
+      color: '#CD521A',
       coordinates: [1.3521, 103.8198],
-      value: 50,
+      lat: 1.3521,
+      lng: 103.8198,
+      size: 50,
     },
 
     {
       id: 'marker2',
       city: 'Indonesia',
-      color: 'red',
+      color: '#CD521A',
       coordinates: [-6.2295712, 106.7594778, 12],
-      value: 50,
+      lat: -6.2295712,
+      lng: 106.7594778,
+      size: 50,
     },
     {
       id: 'marker3',
       city: 'Australia',
-      color: 'red',
+      color: '#CD521A',
       coordinates: [-32.0381872, 115.3872256, 10],
-      value: 50,
+      lat: -32.0381872,
+      lng: 115.3872256,
+      size: 50,
     },
   ]);
 
@@ -53,37 +63,51 @@ const Base = () => {
       }}
     >
       <Meta title={AppConfig.title} description={AppConfig.description} />
-      <Hero />
-      <SizeMe>
+      <SizeMe monitorHeight>
         {({ size: { width } }) => (
-          <Globe
-            ref={globeEl}
-            onGlobeReady={() => {
-              const MAP_CENTER = {
-                lat: -13.3766016,
-                lng: 127.342919,
-                altitude: 1.5,
-              };
-              if (globeEl && globeEl.current) {
-                globeEl?.current?.pointOfView(MAP_CENTER, 100);
-              }
-            }}
-            globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
-            width={width as number}
-            backgroundColor="rgba(0,0,0,0)"
-            onLabelClick={(e: any) => {
-              // eslint-disable-next-line no-alert
-              window.alert(`label click ${JSON.stringify(e)}`);
-            }}
-            labelsData={places}
-            labelLat={(d: any) => (d as any)?.coordinates[0]}
-            labelLng={(d: any) => (d as any)?.coordinates[1]}
-            labelText={(d: any) => (d as any).city}
-            labelSize={() => 2}
-            labelDotRadius={() => 2}
-            labelColor={() => 'rgba(255, 0, 0, 1)'}
-            labelResolution={2}
-          />
+          <>
+            <Hero />
+            <Globe
+              ref={globeEl}
+              onGlobeReady={() => {
+                const MAP_CENTER = {
+                  lat: -13.3766016,
+                  lng: 127.342919,
+                  altitude: 1.5,
+                };
+                if (globeEl && globeEl.current) {
+                  globeEl?.current?.pointOfView(MAP_CENTER, 100);
+                }
+              }}
+              globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
+              width={width as number}
+              backgroundColor="rgba(0,0,0,0)"
+              htmlElementsData={places}
+              htmlElement={(d: any) => {
+                const el = document.createElement('div');
+                el.innerHTML = `
+              <div>
+                <div style='background-color: #FAECD6; position: fixed;
+                top: -15px;
+                left: -120px;
+                padding: 5px 15px;
+                font-weight: bold;
+                border-top-left-radius: 10px;
+                border-bottom-left-radius: 10px;
+                border-top-right-radius: 10px;' >${d.city}</div>
+                <div>${markerSvg}</div>
+              </div>`;
+                el.style.color = '#000';
+                el.style.width = `${d.size}px`;
+
+                // @ts-ignore
+                el.style['pointer-events'] = 'auto';
+                el.style.cursor = 'pointer';
+                el.onclick = () => console.info(d);
+                return el;
+              }}
+            />
+          </>
         )}
       </SizeMe>
       {/* <VerticalFeatures /> */}
