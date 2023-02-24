28175b09bbd67fb2628ca41e01ea196ab508ce03
Guntur KH <guntur.kh@gmail.com>
2023-02-25 01:31:58 +0700

    feat: socmed sidebar

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -3,6 +3,8 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { useRef, useState } from 'react';
 
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { GlobeMethods } from 'react-globe.gl';
 import { SizeMe } from 'react-sizeme';
 
@@ -15,6 +17,7 @@ let Globe: any = () => null;
 if (typeof window !== 'undefined') Globe = require('react-globe.gl').default;
 
 const Base = () => {
+  const router = useRouter();
   const globeEl = useRef<GlobeMethods>();
   const markerSvg = `<svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" clip-rule="evenodd" d="M9.68193 25.0525C2.83316 16.2469 9.10822 3.41669 20.2637 3.41669C31.4191 3.41669 37.6942 16.2469 30.8454 25.0525L20.2637 38.6576L9.68193 25.0525ZM20.2636 22.2084C23.0941 22.2084 25.3886 19.9138 25.3886 17.0834C25.3886 14.2529 23.0941 11.9584 20.2636 11.9584C17.4332 11.9584 15.1386 14.2529 15.1386 17.0834C15.1386 19.9138 17.4332 22.2084 20.2636 22.2084Z" fill="#CD521A"/>
@@ -63,6 +66,52 @@ const Base = () => {
       }}
     >
       <Meta title={AppConfig.title} description={AppConfig.description} />
+      <div className="fixed w-[72px] h-[636px] bg-[#820100] top-[150px] flex flex-col justify-evenly py-10 rounded-tr-3xl rounded-br-3xl shadow z-50">
+        <a
+          href="https://www.facebook.com/gwood.asia.9/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            src={'/assets/images/fb.svg'}
+            alt="Facebook"
+            height={80}
+            width={80}
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/gwood-asia-92493787/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            src={'/assets/images/linkedin.svg'}
+            alt="LinkedIn"
+            height={80}
+            width={80}
+          />
+        </a>
+        <a
+          href="mailto:commercial_admin@gwoodasia.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            src={'/assets/images/email.svg'}
+            alt="Email"
+            height={80}
+            width={80}
+          />
+        </a>
+        <a href="https://wa.me/+6581390383" target="_blank" rel="noreferrer">
+          <Image
+            src={'/assets/images/wa.svg'}
+            alt="Whatsapp"
+            height={80}
+            width={80}
+          />
+        </a>
+      </div>
       <SizeMe monitorHeight>
         {({ size: { width } }) => (
           <>
@@ -87,14 +136,7 @@ const Base = () => {
                 const el = document.createElement('div');
                 el.innerHTML = `
               <div>
-                <div style='background-color: #FAECD6; position: fixed;
-                top: -15px;
-                left: -120px;
-                padding: 5px 15px;
-                font-weight: bold;
-                border-top-left-radius: 10px;
-                border-bottom-left-radius: 10px;
-                border-top-right-radius: 10px;' >${d.city}</div>
+                <div class='hover:bg-[#820100] hover:text-white bg-[#FAECD6] fixed top-[-15px] left-[-120px] py-2 px-4 font-bold rounded-tl-lg rounded-bl-lg rounded-tr-lg'>${d.city}</div>
                 <div>${markerSvg}</div>
               </div>`;
                 el.style.color = '#000';
@@ -103,7 +145,7 @@ const Base = () => {
                 // @ts-ignore
                 el.style['pointer-events'] = 'auto';
                 el.style.cursor = 'pointer';
-                el.onclick = () => console.info(d);
+                el.onclick = () => router.push(d.city.toLowerCase());
                 return el;
               }}
             />
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Gwood Asia',
+  title: 'Gwood Asia Landing Page',
+  description: 'Landing Page Gwood Asia',
   locale: 'en',
 };
