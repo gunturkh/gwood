c8e8c72a0da8a59032e04b585140582869f6735b
Guntur KH <guntur.kh@gmail.com>
2023-03-02 02:43:38 +0700

    feat: wip companies

--- src/pages/companies.tsx
+++ src/pages/companies.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import Image from 'next/image';
+
+import bg from '../../public/assets/images/companies-bg.png';
+import main from '../../public/assets/images/companies-main.png';
+import { Footer } from '../footer/MainFooter';
+import Navbar from '../navigation/Navbar';
+
 function Companies() {
-  return <div>Companies</div>;
+  return (
+    <>
+      <Navbar />
+      <div
+        className="h-[200vh] flex flex-row bg-accent-content"
+        style={{
+          backgroundImage: `url(${bg.src})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          // objectFit: 'contain',
+          width: '100%',
+          height: 'auto',
+          // backgroundAttachment: 'fixed',
+        }}
+      >
+        <div>
+          <p>{' Gwood Supply & Distribution (Pte.) Ltd '}</p>
+          <p>Goodwood Communicators Pty Ltd</p>
+          <p>{'PT Gwood Indonesia (Batam)'}</p>
+          <p>{'PT Gwood Indonesia (Jakarta)'}</p>
+        </div>
+        <Image
+          // layout="fixed"
+          src={main}
+          alt="Company Main"
+          className="w-full"
+          // style={{ marginTop: 120 }}
+        />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Companies;
