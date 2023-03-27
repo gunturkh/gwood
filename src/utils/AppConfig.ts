bf8bcdc0739751b575c69a23d0ec89bb6a06f367
Guntur KH <guntur.kh@gmail.com>
2023-03-27 13:55:19 +0700

    chore: change meta title and desc

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Gwood Asia',
-  title: 'Gwood Asia Landing Page',
-  description: 'Landing Page Gwood Asia',
+  title: 'Gwood Asia Home Page',
+  description: 'Home Page Gwood Asia',
   locale: 'en',
 };
