b2708bb94af896b6fce9ca8f9c426925b61e37ba
Guntur KH <guntur.kh@gmail.com>
2023-07-01 06:50:02 +0700

    feat: basic layout rev wip

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,6 +7,10 @@ import about2 from '../../public/assets/images/about2.png';
 import about3 from '../../public/assets/images/about3.png';
 import about4 from '../../public/assets/images/about4.png';
 import bg from '../../public/assets/images/aboutus.png';
+import capabilities1 from '../../public/assets/images/capabilities1.png';
+import capabilities2 from '../../public/assets/images/capabilities2.png';
+import capabilities3 from '../../public/assets/images/capabilities3.png';
+import capabilities4 from '../../public/assets/images/capabilities4.png';
 import logo from '../../public/assets/images/logo.png';
 import arrow from '../../public/assets/images/scroll-arrow.png';
 import { Footer } from '../footer/MainFooter';
@@ -123,13 +127,13 @@ function About() {
                 </ul>
               </div>
               <div
-                className={`w-[135px] h-[150px] lg:w-[542px] lg:h-[722px] relative group right-0 lg:right-[50px]`}
+                className={`w-[113px] h-[150px] lg:w-[551px] lg:h-[735px] relative group`}
               >
                 <div
-                  className={`gradient-border-mask w-[135px] h-[150px] lg:w-[600px] lg:h-[732px] ease-in duration-100 rounded-3xl backdrop-blur-sm transition-all bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
+                  className={`gradient-border-mask w-[113px] h-[150px] lg:w-[551px] lg:h-[735px] ease-in duration-100 rounded-3xl backdrop-blur-sm transition-all bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
                 ></div>
                 <div
-                  className={`absolute w-[135px] h-[150px] lg:w-[450px] lg:h-[500px] lg:-top-[60px] lg:right-[10px] -top-[20px] -right-[12px]`}
+                  className={`absolute w-[113px] h-[150px] lg:w-[551px] lg:h-[735px] lg:-top-[60px] lg:-right-[80px] -top-[20px] -right-[30px]`}
                 >
                   <img
                     src={about3.src}
@@ -142,21 +146,14 @@ function About() {
           </div>
           <div className="h-[300px] lg:h-screen text-[12px] sm:text-[20px] lg:text-[40px] font-thin text-left mt-8 lg:mt-[50vh]">
             <div className="flex flex-row flex-wrap w-full justify-center items-center">
-              <div className="max-w-[50vw] p-4 lg:p-20 mb-16 ">
-                <p>
-                  Gwood Asia facilitates a gateway for end users, EPCM, project
-                  managers, engineering partners and OEM manufacturers , to
-                  access in-country markets that best supports their objectives
-                </p>
-              </div>
               <div
-                className={`w-[113px] h-[150px] lg:w-[542px] lg:h-[722px] relative group right-0 lg:right-[50px]`}
+                className={`w-[113px] h-[150px] lg:w-[551px] lg:h-[735px] relative group mr-4`}
               >
                 <div
-                  className={`gradient-border-mask w-[113px] h-[150px] lg:w-[542px] lg:h-[722px] ease-in duration-100 rounded-3xl backdrop-blur-sm bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
+                  className={`gradient-border-mask w-[113px] h-[150px] lg:w-[542px] lg:h-[722px] ease-in duration-100 rounded-3xl backdrop-blur-sm transition-all bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
                 ></div>
                 <div
-                  className={`absolute w-[113px] h-[150px] lg:w-[542px] lg:h-[722px] lg:-top-[60px] lg:-right-[80px] -top-[20px] -right-[30px]`}
+                  className={`absolute w-[113px] h-[150px] lg:w-[542px] lg:h-[722px] lg:-top-[60px] lg:-left-[80px] -top-[20px] -left-[30px]`}
                 >
                   <img
                     src={about4.src}
@@ -164,6 +161,132 @@ function About() {
                     className={`w-[113px] h-[150px] lg:w-[542px] lg:h-[722px]`}
                   />
                 </div>
+              </div>
+              <div className="max-w-[50vw] p-4 lg:p-1 mb-16 ">
+                <p>
+                  Gwood Asia facilitates a gateway for end users, EPCM, project
+                  managers, engineering partners and OEM manufacturers , to
+                  access in-country markets that best supports their objectives
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* capabilities */}
+          <div className="h-[500px] md:h-screen w-full">
+            <h1 className="text-right lg:text-[80px] font-semibold mt-0 lg:mt-32">
+              &bull; Capabilities
+            </h1>
+          </div>
+          <div className="h-[300px] md:h-screen text-[12px] sm:text-[20px] lg:text-[40px] font-thin text-left text-white">
+            <div className="flex flex-row flex-wrap w-full justify-center items-center">
+              <div className="max-w-[50vw] p-4 lg:p-20 mb-16 mr-16 ">
+                <p className="font-semibold ">CONTRACTS</p>
+                <ul className="list-disc ml-8 lg:ml-24">
+                  <li>Project Sub-Contracts</li>
+                  <li>Engineering Design</li>
+                  <li>Cost Control Services</li>
+                </ul>
+              </div>
+              <div
+                className={`w-[113px] h-[150px] lg:w-[376px] lg:h-[500px] relative group mr-10`}
+              >
+                <div
+                  className={`gradient-border-mask w-[113px] h-[150px] lg:w-[376px] lg:h-[500px] ease-in duration-100 rounded-3xl bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
+                ></div>
+                <div
+                  className={`absolute w-[113px] h-[150px] lg:w-[376px] lg:h-[500px] lg:-top-[60px] lg:-left-[80px] -top-[20px] -left-[30px]`}
+                >
+                  <img
+                    src={capabilities1.src}
+                    alt="Image 1"
+                    className={`w-[113px] h-[150px] lg:w-[376px] lg:h-[500px]`}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[500px] lg:h-screen text-[12px] md:text-[20px] lg:text-[40px] font-thin text-left mt-48 lg:mt-[50rem] text-white">
+            <div className="flex flex-row flex-wrap w-full justify-center items-center">
+              <div
+                className={`w-[113px] h-[150px] md:w-[376px] md:h-[500px] relative group mr-10`}
+              >
+                <div
+                  className={`gradient-border-mask w-[113px] h-[150px] md:w-[376px] md:h-[500px] ease-in duration-100 rounded-3xl bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
+                ></div>
+                <div
+                  className={`absolute w-[113px] h-[150px] md:w-[376px] md:h-[500px] md:-top-[60px] md:-left-[80px] -top-[20px] -left-[30px]`}
+                >
+                  <img
+                    src={capabilities2.src}
+                    alt="Image 2"
+                    className={`w-[113px] h-[150px] md:w-[376px] md:h-[500px]`}
+                  />
+                </div>
+              </div>
+              <div className="max-w-[50vw] p-4 lg:pl-[12rem] mb-16 ">
+                <p className="font-semibold ">LOGISTICS</p>
+                <ul className="list-disc ml-8 lg:ml-24">
+                  <li>Freight Consignments</li>
+                  <li>Multiple Currency Orders</li>
+                  <li>Commodity Trades</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="h-[500px] lg:h-screen text-[12px] md:text-[20px] lg:text-[40px] font-thin text-left mt-8 lg:mt-48 text-white">
+            <div className="flex flex-row flex-wrap w-full justify-center items-center">
+              <div className="max-w-[50vw] p-4 lg:p-20 mb-16 ">
+                <p className="font-semibold ">COMMERCIAL</p>
+                <ul className="list-disc ml-8 lg:ml-24">
+                  <li>Regional Trade Centre</li>
+                  <li>International Standards Material</li>
+                  <li>Critical Equipment Supply</li>
+                </ul>
+              </div>
+              <div
+                className={`w-[113px] h-[150px] md:w-[376px] md:h-[500px] relative group mr-10`}
+              >
+                <div
+                  className={`gradient-border-mask w-[113px] h-[150px] md:w-[376px] md:h-[500px] ease-in duration-100 rounded-3xl bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
+                ></div>
+                <div
+                  className={`absolute w-[113px] h-[150px] md:w-[376px] md:h-[500px] md:-top-[60px] md:-left-[80px] -top-[20px] -left-[30px]`}
+                >
+                  <img
+                    src={capabilities3.src}
+                    alt="Image 3"
+                    className={`w-[113px] h-[150px] md:w-[376px] md:h-[500px]`}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-[500px] lg:h-screen text-[12px] md:text-[20px] lg:text-[40px] font-thin text-left mt-8 lg:mt-[30rem] text-white">
+            <div className="flex flex-row flex-wrap w-full justify-center items-center">
+              <div
+                className={`w-[113px] h-[150px] md:w-[376px] md:h-[500px] relative group mr-10`}
+              >
+                <div
+                  className={`gradient-border-mask w-[113px] h-[150px] md:w-[376px] md:h-[500px] ease-in duration-100 rounded-3xl bg-white/40 from-white/80 to-white/10 group-hover:bg-white/50 group-hover:bg-gradient-to-br group-hover:from-[#FF6F07]/20 group-hover:via-[#FF6F07]/5 group-hover:to-transparent relative`}
+                ></div>
+                <div
+                  className={`absolute w-[113px] h-[150px] md:w-[376px] md:h-[500px] md:-top-[60px] md:-right-[80px] -top-[20px] -right-[30px]`}
+                >
+                  <img
+                    src={capabilities4.src}
+                    alt="Image 4"
+                    className={`w-[113px] h-[150px] md:w-[376px] md:h-[500px]`}
+                  />
+                </div>
+              </div>
+              <div className="max-w-[50vw] p-4 lg:pl-[12rem] mb-16 ">
+                <p className="font-semibold ">MATERIAL</p>
+                <ul className="list-disc ml-8 lg:ml-24">
+                  <li>Specialised Fabrication</li>
+                  <li>OEM Brand Distribution</li>
+                  <li>Out-Sourced Purchasing</li>
+                </ul>
               </div>
             </div>
           </div>
